97f71cff93941922e5939c826197c866c3f9bf90
FazioNico <contact@nicolasfazio.ch>
2020-08-24 12:51:52 +0200

    upd: add comments

--- src/app/app.js
+++ src/app/app.js
@@ -1,8 +1,13 @@
+// import style CSS
 import './app.css';
+// import HTML template
 import html from './app.html';
 
-const name = 'Parcel Bundler'
+// define data
+const name = 'Parcel Bundler';
+// get HTML tag to build app
 const myApp = document.querySelector('app');
-
+// inject template
 myApp.innerHTML = html;
+// inject data in template
 myApp.querySelector('#name').innerHTML = name;
